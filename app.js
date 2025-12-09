d4d8c409dbe20807de2649b18aafc1eda436acb3
CodeWith-vivek <vivek007anand97@gmail.com>
2025-12-09 16:18:49 +0530

    moved dotenv

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const env = require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const flash = require("connect-flash");
@@ -11,7 +12,6 @@ const userRoute = require("./routes/userRoute");
 const adminRoute=require("./routes/adminRoute")
 
 const app = express();
-const env = require("dotenv").config();
 
 
 db();
